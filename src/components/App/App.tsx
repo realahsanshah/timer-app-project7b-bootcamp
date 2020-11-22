1445faafc9d6b0f7188c043b7bfa264fac3ae9e5
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-11-22 14:37:56 +0500

    Added Basic App components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface AppProps {
+    
+}
+ 
+const App: React.SFC<AppProps> = () => {
+    return ( 
+        <div className="app-container">
+            App
+        </div>
+     );
+}
+ 
+export default App;
